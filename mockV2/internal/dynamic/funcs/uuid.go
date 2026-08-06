69821f6bccc61859d7e2dd969642958d4b214355
DVLI19991130 <jeremiahharrison894@gmail.com>
2026-08-06 11:28:56 +0800

    Delete mockV2 directory

--- mockV2/internal/dynamic/funcs/uuid.go
+++
@@ -1,9 +0,0 @@
-package funcs
-
-import "github.com/google/uuid"
-
-// UUID generates UUID v4
-// $(uuid) -> 550e8400-e29b-41d4-a716-446655440000
-func UUID(args ...string) string {
-	return uuid.New().String()
-}
